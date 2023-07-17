8e5cfaf701f1a7373706959c478e7eaf891a2e8b
Ricky <134692491+RickyLambert87@users.noreply.github.com>
2023-07-17 16:25:41 +0100

    added java and changed some html and css

---
+++ assets/js/scripts.js
@@ -0,0 +1,42 @@
+/**
+ *  Part of responsive navbar code found on W3 schools
+ */
+function navFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+/**
+ * Read more/read less button 
+ */
+function showMore() {
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("read_more");
+
+    if (moreText.style.display === "inline") {
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+
+
+
+/**
+ *  Form button
+ */
+const button = document.querySelector('.btn')
+const form = document.querySelector('.form')
+
+button.addEventListener('click', function () {
+    form.classList.add('form--no')
+});
+
+function signUp() {
+    document.getElementById("form_btn").innerHTML = "Thank You!";
+  }
